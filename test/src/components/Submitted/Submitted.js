4e151519f89c29864fb96a0bac09cd808932b2ae
mollcano <MOLLYJBROOKS@GMAIL.COM>
2017-09-24 09:35:54 -0600

    heat index working

--- test/src/components/Submitted/Submitted.js
+++ test/src/components/Submitted/Submitted.js
@@ -40,7 +40,7 @@ export default class Submitted extends Component {
              <h5><b>Risks:</b> {form.riskname}</h5>
              <h5><b>Mitigation Methods:</b> {form.mitigationname}</h5>
              <h5><b>Threat Level of Risk:</b></h5>
-             <input className = "heatindex" type = "range" min="0" max="1" disabled value = {form.riskidentified.heatindex}></input>
+             <input className = "heatindex" type = "range" min="0" max="100" disabled value = {form.riskidentified.heatindex*100}></input>
              <h5><b>Rate this FLRA:</b></h5>
              {/* talk to bill where to send action */}
              <form action="#" method="update" id={form.employeename + i}><input name="supervisorscore" className = "supervisorscore" type = "range" min="0" max="10" defaultValue = "5" id ={ i }></input><div className="indents"><p className="indent">0</p><p className="indent">1</p><p className="indent">2</p><p className="indent">3</p><p className="indent">4</p><p className="indent">5</p><p className="indent">6</p><p className="indent">7</p><p className="indent">8</p><p className="indent">9</p><p className="indent">10</p></div></form>
