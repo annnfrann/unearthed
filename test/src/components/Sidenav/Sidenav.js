6f28a341b91bf134d2f9de90e3f99686003d9e3d
mollcano <MOLLYJBROOKS@GMAIL.COM>
2017-09-23 22:45:01 -0600

    added icons

--- test/src/components/Sidenav/Sidenav.js
+++ test/src/components/Sidenav/Sidenav.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-import {SideNav, Button, SideNavItem} from 'react-materialize'
+import {SideNav, Button, SideNavItem, Icon} from 'react-materialize'
 import './sidenav.css';
 
 export default class Sidenav extends Component{
   render(){
     return(
       <SideNav id = "slide-out" className = "side-nav fixed" trigger={<p> </p>} >
-        <SideNavItem href = '#!icon' ><div className = "white-text"> Home </div> < /SideNavItem>
-        <SideNavItem href = '#!second' ><div className = "white-text"> Activated </div> < /SideNavItem>
-        <SideNavItem href = '#!third' ><div className = "white-text"> Forms </div> < /SideNavItem>
-        <SideNavItem href = '#!fourth' ><div className = "white-text"> Staff Reports </div> < /SideNavItem>
+        <SideNavItem href = '#!icon' ><div className = "white-text"><Icon>home</Icon> Home </div> < /SideNavItem>
+
+        <SideNavItem href = '#!second' ><div className = "white-text"><Icon>work</Icon> Tasks </div> < /SideNavItem>
+
+        <SideNavItem href = '#!third' ><div className = "white-text"><Icon>insert_drive_file</Icon> Forms </div> < /SideNavItem>
+  
+        <SideNavItem href = '#!fourth' ><div className = "white-text"><Icon>face</Icon> Staff Reports </div> < /SideNavItem>
         <hr />
-        <SideNavItem href = '#!fifth' ><div className = "white-text"> Settings </div> < /SideNavItem>
+        <SideNavItem href = '#!fifth' ><div className = "white-text"><Icon>settings</Icon> Settings </div> < /SideNavItem>
       < /SideNav>
     );
   }
