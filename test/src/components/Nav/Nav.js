73b30bee858c41bd8b9fe63226611fde94a5ea8a
mollcano <MOLLYJBROOKS@GMAIL.COM>
2017-09-24 09:22:39 -0600

    pulling supervisor from database

--- test/src/components/Nav/Nav.js
+++ test/src/components/Nav/Nav.js
@@ -2,12 +2,36 @@ import React, { Component } from 'react';
 import '../../index.css';
 import './Nav.css'
 import { Navbar, NavItem } from 'react-materialize';
+import axios from 'axios'
 
-export default class Nav extends Component{
+const URL = "http://ec2-54-201-167-124.us-west-2.compute.amazonaws.com:8080/supervisor/123";
+
+export default class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      supervisor: ''
+    }
+  }
+
+  componentDidMount() {
+    var _this = this;
+    axios.get(URL)
+    .then(function(res){
+      console.log(res)
+      _this.setState({
+        supervisor: res.data[0].supervisorname
+      });
+    })
+    .catch(function(e) {
+      console.log("ERROR ", e);
+    })
+  }
   render(){
     return(
       <Navbar className="myNav" brand="&nbsp;&nbsp;&nbsp;&nbsp;FLRApp" right fixed>
-        <NavItem href='#' className='supervisor'>Mr. Supervisor</NavItem>
+
+        <NavItem href='#' className='supervisor'>{this.state.supervisor}</NavItem>
       </Navbar>
     );
   }
